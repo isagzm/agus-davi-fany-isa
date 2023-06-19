5d4bc9a5a364a240419b79eeeccfcfec9f67c2c2
Isabel González <isaabelglez@gmail.com>
2023-06-19 17:34:37 +0100

    add works test

--- src/bank-kata/account.test.js
+++ src/bank-kata/account.test.js
@@ -2,23 +2,32 @@ import { describe, it, expect } from "vitest";
 import { Account } from "./account.js";
 import { Logger } from "./account.js";
 
+class LoggerFake extends Logger {
+  constructor() {
+    super();
+    this.message = [];
+  }
+  log(message) {
+    this.message.push(message);
+  }
+}
+
 describe("Account", () => {
-    it("works with the example", () => {
-        const logger = new Logger();
-        const account = new Account(logger);
+  it("works with the example", () => {
+    const logger = new LoggerFake();
+    const account = new Account(logger);
 
-        account.deposit(1000);
-        account.deposit(2000);
-        account.withdraw(500);
+    account.deposit(1000);
+    account.deposit(2000);
+    account.withdraw(500);
 
-        account.printStatement();
+    account.printStatement();
 
-        expect(logger.message).toEqual([
-            "DATE | AMOUNT | BALANCE",
-            "24/01/2012 | 500.00 | 2500.00",
-            "23/01/2012 | 2000.00 | 3000.0",
-            "20/01/2012 | 1000.00 | 1000.0",   
-        ])
-    })
-} )
-
+    expect(logger.message).toEqual([
+      "DATE | AMOUNT | BALANCE",
+      "24/01/2012 | 500.00 | 2500.00",
+      "23/01/2012 | 2000.00 | 3000.0",
+      "20/01/2012 | 1000.00 | 1000.0",
+    ]);
+  });
+});
